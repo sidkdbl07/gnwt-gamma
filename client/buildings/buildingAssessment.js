1d3448d8d7cae00b401bfd7bec61f3e930221448
Sid Kwakkel <kwakkels@ae.ca>
2017-02-28 22:35:22 -0800

    Assessment navigation between groups is functional. Need to render and answer questions now

--- client/buildings/buildingAssessment.js
+++ client/buildings/buildingAssessment.js
@@ -21,7 +21,36 @@ Template.buildingAssessment.helpers({
   },
   getElementGroup: function(){
     return AssessmentElementGroups.findOne({assessment_id: FlowRouter.getParam('assessmentId'), order: parseInt(FlowRouter.getParam('order'))});
-  }
+  },
+  nextGroup: function() {
+    return "/building/assessment/"+FlowRouter.getParam('buildingId')+"/"+FlowRouter.getParam('assessmentId')+"/"+(parseInt(FlowRouter.getParam('order')) + 1);
+  },
+  nextGroupExists: function() {
+    var next_order = parseInt(FlowRouter.getParam('order')) + 1;
+    var next_group_count = AssessmentElementGroups.find({assessment_id: FlowRouter.getParam('assessmentId'), order: next_order}).count();
+    if(next_group_count > 0) {
+      return true;
+    }
+    return false;
+  },
+  pageText: function() {
+    var number_of_pages = AssessmentElementGroups.find({assessment_id: FlowRouter.getParam('assessmentId')}).count();
+    return "Page "+(parseInt(FlowRouter.getParam('order')) + 1)+" of "+number_of_pages;
+  },
+  previousGroup: function() {
+    return "/building/assessment/"+FlowRouter.getParam('buildingId')+"/"+FlowRouter.getParam('assessmentId')+"/"+(parseInt(FlowRouter.getParam('order')) - 1);
+  },
+  previousGroupExists: function() {
+    if(parseInt(FlowRouter.getParam('order')) == 0) {
+      return false;
+    }
+    var next_order = parseInt(FlowRouter.getParam('order')) - 1;
+    var next_group_count = AssessmentElementGroups.find({assessment_id: FlowRouter.getParam('assessmentId'), order: next_order}).count();
+    if(next_group_count > 0) {
+      return true;
+    }
+    return false;
+  },
 });
 
 Template.buildingAssessment.events({
@@ -29,5 +58,15 @@ Template.buildingAssessment.events({
 
   }
 });
+
+Template.buildingAssessment.onRendered(function() {
+  if(Meteor.isCordova){
+    $(".content").css('height', '672px');
+    $(".content").css('max-height', '672px');
+  }else{
+    $(".content").css('height', '672px');
+    $(".content").css('max-height', '672px'); //778
+  }
+})
 
 }
